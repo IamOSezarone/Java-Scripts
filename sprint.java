fe3bc24e526fe71394a8970a8ce0c76526f3f62c
Sezarone <omarsezarone@gmail.com>
2017-05-23 03:35:23 +0200

    Simple Print Script

---
+++ sprint.java
@@ -0,0 +1,9 @@
+// S E Z A R (1);
+// [ Copyright Dev-TALENT (C) All rights reserved 2015 - 2017 ]
+
+class SimplePrint {
+  public void main(String []args) {
+    String lol='This Simple Java Script';
+    System.out.printIn(lol);
+   }
+}
